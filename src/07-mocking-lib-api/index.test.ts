071b625ae86feb31bd9ce32b9ef283eaaf1a3aab
Kostiantyn Sakharov <hellraisercw@gmail.com>
2024-02-12 12:55:42 +0200

    fix: lib api fix

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,41 +1,52 @@
 import axios from 'axios';
 
-import { throttledGetDataFromApi } from './index';
+import { THROTTLE_TIME, throttledGetDataFromApi } from './index';
 
-jest.mock('lodash', () => {
-  const originalModule = jest.requireActual('lodash');
-
-  return {
-    __esModule: true,
-    ...originalModule,
-    throttle: jest.fn((fn) => fn),
-  };
-});
-
+const testURLConfig = {
+  baseURL: 'https://jsonplaceholder.typicode.com'
+};
 const relativePath = 'users';
+const mockData = {
+  data: 'Some mock data'
+};
 
 describe('throttledGetDataFromApi', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   test('should create instance with provided base url', async () => {
     const spy = jest.spyOn(axios, 'create');
 
     await throttledGetDataFromApi(relativePath);
 
-    expect(spy).toHaveBeenCalledWith({
-      baseURL: 'https://jsonplaceholder.typicode.com',
-    });
+    expect(spy).toHaveBeenCalledWith(testURLConfig);
   });
 
   test('should perform request to correct provided url', async () => {
-    const spy = jest.spyOn(axios.create(), 'get');
+    const testAxiosConfig = axios.create(testURLConfig);
+    const spy = jest.spyOn(testAxiosConfig, 'get');
+    jest.spyOn(axios, 'create').mockReturnValue(testAxiosConfig);
 
     await throttledGetDataFromApi(relativePath);
+    jest.advanceTimersByTime(THROTTLE_TIME);
 
     expect(spy).toHaveBeenCalledWith(relativePath);
   });
 
   test('should return response data', async () => {
-    const result = await throttledGetDataFromApi(relativePath);
+    const testAxiosConfig = axios.create(testURLConfig);
+    const spy = jest.spyOn(testAxiosConfig, 'get').mockResolvedValue(mockData);
+    jest.spyOn(axios, 'create').mockReturnValue(testAxiosConfig);
 
-    expect(result).toBe('response');
+    const result = await throttledGetDataFromApi(relativePath);
+    jest.advanceTimersByTime(THROTTLE_TIME);
+
+    expect(spy).toHaveBeenCalled();
+    expect(result).toBe(mockData.data);
   });
 });
